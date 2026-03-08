d5fd38060f944f8b7064e661d51402f18dc87d40
codingjordi <jordisaperasc@gmail.com>
2026-03-08 21:46:16 +0100

    Exercise Part08_04.LiquidContainers2

---
+++ part08-Part08_04.LiquidContainers2/src/main/java/Container.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+
+public class Container {
+
+    private int amount;
+
+    public Container() {
+        this.amount = 0;
+    }
+
+    public int contains() {
+        return this.amount;
+    }
+
+    public void add(int amount) {
+        if (amount < 0) {
+            return;
+        }
+
+        this.amount += amount;
+
+        if (this.amount > 100) {
+            this.amount = 100;
+        }
+    }
+
+    public void remove(int amount) {
+        if (amount < 0) {
+            return;
+        }
+
+        this.amount -= amount;
+
+        if (this.amount < 0) {
+            this.amount = 0;
+        }
+    }
+
+    public void move(Container other, int amount) {
+
+        if (amount < 0) {
+            return;
+        }
+
+        if (amount > this.amount) {
+            amount = this.amount;
+        }
+
+        this.remove(amount);
+        other.add(amount);
+    }
+
+    public String toString() {
+        return this.amount + "/100";
+    }
+}
