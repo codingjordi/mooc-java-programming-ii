73cf6d6752fef3357d54ca7d93dcb9bd248cf3b1
codingjordi <jordisaperasc@gmail.com>
2026-03-10 21:31:32 +0100

    Exercise Part08_06.Nicknames fix

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,13 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> names = new HashMap<>();
+        
+        names.put("matt", "matthew");
+        names.put("mix", "michael");
+        names.put("artie", "arthur");
+        
+        System.out.println(names.get("matt"));
     }
 
 }
