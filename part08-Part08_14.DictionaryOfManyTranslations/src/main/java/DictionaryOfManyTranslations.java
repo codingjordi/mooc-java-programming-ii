b718050d0b077d828c1dea43a14e9a025ac547d2
codingjordi <jordisaperasc@gmail.com>
2026-03-20 17:54:27 +0100

    Exercise Part08_14.DictionaryOfManyTranslations

---
+++ part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Admin
+ */
+public class DictionaryOfManyTranslations {
+
+    private HashMap<String, ArrayList<String>> translations;
+
+    public DictionaryOfManyTranslations() {
+        this.translations = new HashMap<>();
+    }
+
+    public void add(String word, String translation) {
+
+        this.translations.putIfAbsent(word, new ArrayList<>());
+
+        this.translations.get(word).add(translation);
+
+    }
+
+    public ArrayList<String> translate(String word) {
+
+        return this.translations.getOrDefault(word, new ArrayList<>());
+
+    }
+
+    public void remove(String word) {
+
+        if (this.translations.get(word) instanceof ArrayList) {
+            this.translations.remove(word);
+        }
+
+    }
+}
