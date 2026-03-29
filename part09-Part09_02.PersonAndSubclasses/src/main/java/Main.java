5a42f9e787d0aa15fc4898ee5d33f99b6c9f4b76
codingjordi <jordisaperasc@gmail.com>
2026-03-29 16:39:37 +0200

    Exercise Part09_02.PersonAndSubclasses 5/5

--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,22 +1,21 @@
 
+import java.util.ArrayList;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your test code here
-        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-        System.out.println(ada);
-        System.out.println(esko);
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
-        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
-        int i = 0;
-        while (i < 25) {
-            ollie.study();
-            i = i + 1;
+        printPersons(persons);
+    }
+    
+    public static void printPersons(ArrayList<Person> persons) {
+        for(Person person : persons) {
+            System.out.println(person);
         }
-        System.out.println(ollie);
-
     }
 
 }
