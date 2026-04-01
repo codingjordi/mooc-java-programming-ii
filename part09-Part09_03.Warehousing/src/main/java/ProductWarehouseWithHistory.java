5ecce32f1df4007da3addefc0e7dfe09c63f0eee
codingjordi <jordisaperasc@gmail.com>
2026-04-01 18:30:30 +0200

    Exercise Part09_03.Warehousing 5/7

---
+++ part09-Part09_03.Warehousing/src/main/java/ProductWarehouseWithHistory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jordi
+ */
+public class ProductWarehouseWithHistory extends ProductWarehouse {
+
+    private ChangeHistory history;
+
+    public ProductWarehouseWithHistory(String productName, double capacity, double initialBalance) {
+        super(productName, capacity);
+        this.history = new ChangeHistory();
+
+        super.addToWarehouse(initialBalance);
+
+        this.history.add(initialBalance);
+    }
+
+    public String history() {
+        return this.history.toString();
+    }
+}
