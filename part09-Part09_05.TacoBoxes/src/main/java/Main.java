5c46de10791f725d12aa7a323cc9fb9507fc6409
codingjordi <jordisaperasc@gmail.com>
2026-04-18 22:46:50 +0200

    Exercise Part09_05.TacoBoxes 1/2

---
+++ part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Test your code here!
+        
+        TripleTacoBox tacobox = new TripleTacoBox();
+        
+        tacobox.eat();
+        tacobox.eat();
+        
+        System.out.println(tacobox.tacosRemaining());
+    }
+}
